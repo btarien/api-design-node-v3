7120f8e299ef7f16494f1f621827ccfa35065974
Britt Tarien <britt.tarien@gmail.com>
2021-07-17 15:47:25 +0200

    lesson 5

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,61 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!(req.body.email && req.body.password)) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
+  try {
+    const user = await User.create(req.body)
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (err) {
+    console.error(err)
+    return res.status(400).end()
+  }
+}
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {
+  if (!(req.body.email && req.body.password)) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
+  const user = await User.findOne({ email: req.body.email }).exec()
+  if (!user) {
+    return res.status(401).send({ message: 'no such user' })
+  }
+  try {
+    const match = await user.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'incorrect password' })
+    }
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (err) {
+    console.error(err)
+    return res.status(401).send({ message: 'invalid login credentials' })
+  }
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  const bearer = req.headers.authorization
+
+  if (!bearer || !bearer.startsWith('Bearer ')) {
+    return res.status(401).end()
+  }
+  let token = bearer.split('Bearer ')[1]
+  if (!token) {
+    return res.status(401).end()
+  }
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+    next()
+  } catch (err) {
+    console.error(err)
+    return res.status(401).end()
+  }
 }
