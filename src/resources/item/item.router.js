a1150bd47663177715b04f214d5af9b92ca2dcfa
Britt Tarien <britt.tarien@gmail.com>
2021-07-17 05:34:50 +0200

    lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,25 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => {
+    res.send({ message: 'get item' })
+  })
+  .post((req, res) => {
+    res.send({ message: 'post item' })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send({ message: 'get item with id' })
+  })
+  .put((req, res) => {
+    res.send({ message: 'delete item with id' })
+  })
+  .delete((req, res) => {
+    res.send({ message: 'post item with id' })
+  })
+
 export default router
